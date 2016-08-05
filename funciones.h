0f1b290289df06b2cbb3dfe187525e486e191aef
sinozuke <songokusinozuke@gmail.com>
2016-08-04 21:11:52 -0600

    commit numero 8 :P

--- funciones.h
+++ funciones.h
@@ -4,6 +4,10 @@
 
 void generar_disco(char *nombre,char *path, int tamano, int tipo);
 void eliminar_disco(char *path);
-
+void crear_pp();
+void crear_pl();
+void crear_pe();
+void mount_c();
+void umount_c();
 
 #endif // FUNC_GENERACION_H_INCLUDE
