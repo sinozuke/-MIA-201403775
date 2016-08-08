a11fe87f3af480df482c31a008f2d4a6651e8a21
sinozuke <songokusinozuke@gmail.com>
2016-08-08 02:01:18 -0600

    funcion 'mkdisk' completa version final 1

--- funciones.c
+++ funciones.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <time.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include "Datos.h"
 #include "funciones.h"
@@ -9,11 +11,7 @@
 int crear_directorios(char *path);
 
 void generar_disco(char *nombre,char *path, int tamano, int tipo){
-/*    MBR mbr1;
-    mbr1.mbr_fecha_creacion = time(NULL);
-    printf("hecho!\n%");
-    printf(ctime(&mbr1.mbr_fecha_creacion));
-*/
+
     char directorio[strlen(path)];
     strcpy(directorio,path);
     if(crear_directorios(path)==0){
@@ -40,18 +38,61 @@ void generar_disco(char *nombre,char *path, int tamano, int tipo){
         return;
     }
 
-    printf("%i\n",tipo);
-
     if(tipo==1)
         for(int j=0;j<tamano;j++)
             for(int i=0;i<1024;i++)
                 fwrite(KiloByte,sizeof(char),1024,disco);
-    else
+     else
         for(int i=0;i<tamano;i++)
                 fwrite(KiloByte,sizeof(char),1024,disco);
 
+    srand((unsigned int)time(NULL));
+
+    MBR nuevo_mbr;
+    nuevo_mbr.mbr_fecha_creacion = time(NULL);
+
+    if(tipo==1)
+        nuevo_mbr.mbr_tamano = 1024*1024*tamano;
+    else
+        nuevo_mbr.mbr_tamano = 1024*tamano;
+
+    particion particion_vacia;
+
+    particion_vacia.part_status = 'n';
+    particion_vacia.part_type = 'n';
+    particion_vacia.part_fit = 'n';
+    particion_vacia.part_start = -1;
+    particion_vacia.part_size = 0;
+
+    nuevo_mbr.mbr_partition_1 = particion_vacia;
+    nuevo_mbr.mbr_partition_2 = particion_vacia;
+    nuevo_mbr.mbr_partition_3 = particion_vacia;
+    nuevo_mbr.mbr_partition_4 = particion_vacia;
+
+    nuevo_mbr.mbr_disk_signature = rand();
+
+    fseek(disco,0,SEEK_SET);
+
+    fwrite(&nuevo_mbr,sizeof(MBR),1,disco);
+
     fclose(disco);
     printf("************GENERACION COMPLETA************\n\n");
+/*
+    disco = fopen(nombred,"r+b");
+
+    if(!disco){
+        printf("ERROR: el Disco no ha podido crearse.\n");
+        printf("************GENERACION FALLIDA************\n\n");
+        return;
+    }
+
+    MBR devuelto;
+
+    if(fread(&devuelto,sizeof(MBR),1,disco)==1)
+        printf("%i\n",devuelto.mbr_tamano);
+    else
+        printf("ERROR:al cargar la data.\n");
+*/
 }
 
 void eliminar_disco(char *path){
